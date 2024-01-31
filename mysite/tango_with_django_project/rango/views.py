fa81c0ad62e20b40e18f89eca4ac98d01f8c0da2
Taewan Kim <2798186k@student.gla.ac.uk>
2024-02-01 08:23:37 +0900

    Final upload Chapter Exercise

--- mysite/tango_with_django_project/rango/views.py
+++ mysite/tango_with_django_project/rango/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    response = '<a href = "about/">"About"</a> Rango says hey there partner'
-    return HttpResponse(response)
+    return HttpResponse('<a href="/rango/about/">About</a>"Rango says hey there partner!"')
 
 def about(request):
-    response = '<a href = "/rango">"Index"</a> "Rango says here is the about page"'
-    return HttpResponse(response)
+    return HttpResponse('<a href="/rango/">Index</a>"Rango says here is the about page."')
